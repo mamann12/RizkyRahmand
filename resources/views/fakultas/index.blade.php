d2d20287b5d81a0e8b4c5316b9b2391fab748db5
mamann12 <rizkyrahmand@gmail.com>
2023-11-07 15:08:59 +0700

    update

--- resources/views/fakultas/index.blade.php
+++ resources/views/fakultas/index.blade.php
@@ -1,17 +1,27 @@
 @extends('layout.main')
-@section('title','Fakultas')
+@section('title', 'Fakultas')
 
 @section('content')
 
-    <h1>Halaman Fakultas</h1>
-    <table class="table table-striped">
-        <tr>
-            <th>Nama Fakultas</th>
-        </tr>
-        @foreach ($fakultas as $item )
-        <tr>
-            <td>{{ $item['nama']}}</td>
-        </tr>
-        @endforeach
-    </table>
+    <div class="row">
+        <div class="col-lg-12 grid-margin stretch-card">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">Fakultas</h4>
+                    <p class="card-description">
+                        Daftar Fakultas di Universitas MDP
+                    </p>
+                    <div class="table-responsive">
+                        <table class="table table-hover table-stripped"></table>
+                        <thead>
+                            <tr>
+                                <th>Nama Fakultas</th>
+                            </tr>
+                        </thead>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
